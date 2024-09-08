6140dc53124a487d5159a53943cddd0e2013390e
Enio Lee <eniolee@MacBook-Air-de-Enio.local>
2024-09-08 13:07:01 -0300

    chore: setup inicial

---
+++ terraform/variables.tf
@@ -0,0 +1,76 @@
+#
+# Region
+# 
+variable "region" {
+  type = string
+}
+
+#
+# Service name
+# 
+variable "service_name" {
+
+}
+
+#
+# Cluster name
+# 
+variable "cluster_name" {
+
+}
+
+#
+# Service port
+# 
+variable "service_port" {
+
+}
+
+#
+# Service cpu
+# 
+variable "service_cpu" {
+
+}
+
+#
+# Service memory
+# 
+variable "service_memory" {
+
+}
+
+#
+# SSM listener
+# 
+variable "ssm_listener" {
+
+}
+
+#
+# SSM vpc id
+# 
+variable "ssm_vpc_id" {
+
+}
+
+#
+# Private subnet 1
+# 
+variable "ssm_private_subnet_1" {
+
+}
+
+#
+# Private subnet 2
+#
+variable "ssm_private_subnet_2" {
+
+}
+
+#
+# Private subnet 3
+#
+variable "ssm_private_subnet_3" {
+
+}
