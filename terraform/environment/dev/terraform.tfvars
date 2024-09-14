3111f116d04a888d9d0d5bf3c3c4e55fc5c25d9c
Enio Lee <eniolee@MacBook-Air-de-Enio.local>
2024-09-14 12:44:32 -0300

    Dia 4

--- terraform/environment/dev/terraform.tfvars
+++ terraform/environment/dev/terraform.tfvars
@@ -11,14 +11,14 @@ cluster_name = "linuxtips-ecs-cluster"
 #
 # Service
 # 
-service_name   = "chip"
-service_port   = 8080
-service_cpu    = 256
-service_memory = 512
-service_launch_type = "EC2"
-service_task_count = 3
+service_name             = "chip"
+service_port             = 8080
+service_cpu              = 256
+service_memory           = 512
+service_launch_type      = "EC2"
+service_task_count       = 3
 service_sg_ingress_ports = ["8080"]
-service_hosts = ["chip.linuxtips.demo"]
+service_hosts            = ["chip.linuxtips.demo"]
 
 #
 # SSM values
@@ -55,3 +55,28 @@ service_healthcheck = {
   path                = "/healthcheck"
   port                = 8080
 }
+
+scale_type   = "requests_tracking"
+task_minimum  = 3
+task_maximum = 12
+
+scale_out_cpu_threshold       = 50
+scale_out_adjustment          = 2
+scale_out_comparison_operator = "GreaterThanEqualToThreshold"
+scale_out_statistic           = "Average"
+scale_out_period              = 60
+scale_out_evaluation_periods  = 2
+scale_out_cooldown            = 60
+
+scale_in_cpu_threshold       = 30
+scale_in_adjustment          = -1
+scale_in_comparison_operator = "LessThanOrEqualToThreshold"
+scale_in_statistic           = "Average"
+scale_in_period              = 60
+scale_in_evaluation_periods  = 2
+scale_in_cooldown            = 60
+
+scale_tracking_cpu      = 50
+scale_tracking_requests = 20
+
+ssm_alb = "/linuxtips/ecs/lb/arn"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,125 +1,119 @@
 #
 # Region
 # 
-variable "region" {
-  type = string
-}
+variable "region" {}
 
 #
 # Service name
 # 
-variable "service_name" {
-
-}
+variable "service_name" {}
 
 #
 # Cluster name
 # 
-variable "cluster_name" {
-
-}
+variable "cluster_name" {}
 
 #
 # Service port
 # 
-variable "service_port" {
-
-}
+variable "service_port" {}
 
 #
 # Service cpu
 # 
-variable "service_cpu" {
-
-}
+variable "service_cpu" {}
 
 #
 # Service memory
 # 
-variable "service_memory" {
-
-}
+variable "service_memory" {}
 
 #
-# SSM listener
-# 
-variable "ssm_listener" {
-
-}
-
+# From SSM
 #
-# SSM vpc id
-# 
-variable "ssm_vpc_id" {
-
-}
-
-#
-# SSM Private subnet 1
-# 
-variable "ssm_private_subnet_1" {
-
-}
-
-#
-# SSM Private subnet 2
-#
-variable "ssm_private_subnet_2" {
-
-}
-
-#
-# SSM Private subnet 3
-#
-variable "ssm_private_subnet_3" {
-
-}
+variable "ssm_listener" {}
+variable "ssm_vpc_id" {}
+variable "ssm_private_subnet_1" {}
+variable "ssm_private_subnet_2" {}
+variable "ssm_private_subnet_3" {}
+variable "ssm_alb" {}
 
 #
 # Environment
 #
-variable "environment_variables" {
-
-}
+variable "environment_variables" {}
 
 #
 # Capabilities
 #
-variable "capabilities" {
-
-}
+variable "capabilities" {}
 
 #
 # Service healthcheck
 #
-variable "service_healthcheck" {
-
-}
+variable "service_healthcheck" {}
 
 #
 # Service launch type
 #
-variable "service_launch_type" {
-
-}
+variable "service_launch_type" {}
 
 #
 # Service task count
 #
-variable "service_task_count" {
-
-}
+variable "service_task_count" {}
 
 #
 # Service ingress ports
 #
-variable "service_sg_ingress_ports" {
-
-}
+variable "service_sg_ingress_ports" {}
 
 #
 # Service hosts
 #
-variable "service_hosts" {
+variable "service_hosts" {}
 
-}
+
+variable "scale_type" {}
+
+variable "task_minimum" {}
+
+variable "task_maximum" {}
+
+#
+# Task Auto Scaling CPU
+#
+variable "scale_out_cpu_threshold" {}
+
+variable "scale_out_adjustment" {}
+
+variable "scale_out_comparison_operator" {}
+
+variable "scale_out_statistic" {}
+
+variable "scale_out_period" {}
+
+variable "scale_out_evaluation_periods" {}
+
+variable "scale_out_cooldown" {}
+
+variable "scale_in_cpu_threshold" {}
+
+variable "scale_in_adjustment" {}
+
+variable "scale_in_comparison_operator" {}
+
+variable "scale_in_statistic" {}
+
+variable "scale_in_period" {}
+
+variable "scale_in_evaluation_periods" {}
+
+variable "scale_in_cooldown" {}
+
+#
+# Task Auto Scaling tracking
+#
+variable "scale_tracking_cpu" {}
+
+variable "scale_tracking_requests" {}
